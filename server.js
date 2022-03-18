5db575b9d0db26951c55ea6bd799fd501dadc8be
Kaitlin Berryman <kaitlinberryman@gmail.com>
2022-03-18 13:55:37 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express') 
+const app = express()
+
+app.use(logger)
+
+app.get('/', (req, res) => {
+  console.log('Home')
+  res.send('Home Page')
+})
+
+app.get('/users', (req, res) => {
+  console.log('Users')
+  res.send('Users Page')
+})
+
+function logger(req, res, next) {
+  console.log('Log')
+  next()
+}
